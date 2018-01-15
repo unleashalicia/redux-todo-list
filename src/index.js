7042521fae584b90a8777dcebabd278e137da037
unleashalicia <alicia.adams.evans@gmail.com>
2018-01-15 12:11:42 -0800

    Added own middleware

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
-import promise from 'redux-promise';
+// import promise from 'redux-promise';
+import logger from './middleware/logger';
+import promise from './middleware/promise';
 
 import App from './components/app';
 
 ReactDOM.render(
-    <Provider store={createStore(rootReducer, {}, applyMiddleware(promise))}>
+    <Provider store={createStore(rootReducer, {}, applyMiddleware(promise, logger))}>
         <Router>
             <App />
         </Router>
